6b10ad1172dd5b1d0ad96e44a3f4f3e18fa83885
Kees Meijer <keesie.meijer@gmail.com>
2017-06-09 15:01:43 +0200

    Use grunt tasks to update version

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function( grunt ) {
+
+	require( 'load-grunt-tasks' )( grunt );
+
+	'use strict';
+
+	// Project configuration
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		addtextdomain: {
+			options: {
+				textdomain: 'custom-post-type-cleanup',
+			},
+			target: {
+				files: {
+					src: [ '*.php', '**/*.php', '!node_modules/**', '!bin/**' ]
+				}
+			}
+		},
+
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages',
+					mainFile: 'custom-post-type-cleanup.php',
+					potFilename: 'custom-post-type-cleanup.pot',
+					potHeaders: {
+						poedit: true,
+						'x-poedit-keywordslist': true
+					},
+					type: 'wp-plugin',
+					updateTimestamp: true
+				}
+			}
+		},
+
+		version: {
+			readmetxt: {
+				options: {
+					prefix: 'Stable tag: *'
+				},
+				src: [ 'readme.txt' ]
+			},
+			tested_up_to: {
+				options: {
+					pkg: {
+						"version": "<%= pkg.tested_up_to %>"
+					},
+					prefix: 'Tested up to: *'
+				},
+				src: [ 'readme.txt', 'readme.md' ]
+			},
+			requires_at_least: {
+				options: {
+					pkg: {
+						"version": "<%= pkg.requires_at_least %>"
+					},
+					prefix: 'Requires at least: *'
+				},
+				src: [ 'readme.txt', 'readme.md' ]
+			},
+			plugin: {
+				options: {
+					prefix: 'Version: *'
+				},
+				src: [ 'readme.md', 'custom-post-type-cleanup.php' ]
+			},
+		},
+	} );
+
+
+	grunt.registerTask( 'i18n', [ 'addtextdomain', 'makepot' ] );
+
+	// Creates build
+	grunt.registerTask( 'build', [ 'version', 'makepot' ] );
+
+	grunt.util.linefeed = '\n';
+
+};
---
+++ readme.txt
@@ -0,0 +1,19 @@
+=== Tosti ===
+Contributors: keesiemeijer
+Requires at least: 4.0
+Tested up to: 4.8
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Detect and delete posts from custom post types that are no longer in use 
+
+== Description ==
+
+Custom post type posts are left in the database if a post type is no longer registered (in use).
+
+Plugins and themes can (without you knowing) use custom post types as a way to store data. These posts stay in the database forever if they're not cleaned up by the plugin/theme itself upon deletion.
+
+There are a lot of plugins that clean your database (by removing revisions, drafts etc.), but I haven't found one that does a cleanup of unused post type posts. 
+
+This plugin provides an easy way to detect and remove posts from post types that are no longer in use. The settings page for this plugin is at wp-admin > Tools > Custom Post Type Cleanup.
