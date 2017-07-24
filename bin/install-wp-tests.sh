e686cbea029ccd104b027fc5aac054588a098218
Kees Meijer <keesie.meijer@gmail.com>
2017-07-24 18:07:27 +0200

    Remove protocol

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -114,7 +114,7 @@ install_db() {
 		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
 			EXTRA=" --socket=$DB_SOCK_OR_PORT"
 		elif ! [ -z $DB_HOSTNAME ] ; then
-			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
+			EXTRA=" --host=$DB_HOSTNAME"
 		fi
 	fi
 
