28b299cb1b9de8019a0d3d51a6c6fc2c825b9922
Kees Meijer <keesie.meijer@gmail.com>
2017-07-26 17:54:50 +0200

    Add test for deleting correct post type

--- tests/test-delete.php
+++ tests/test-delete.php
@@ -20,10 +20,25 @@ class CPTC_Test_Delete extends CPTC_Post_Type_Cleanup_UnitTestCase {
 	function test_admin_page_deleting_posts_in_batches() {
 		$posts = $this->create_not_registered_post_type_posts( 'cpt', 10 );
 		$this->assertEquals( 10, count( get_posts( 'post_type=cpt&posts_per_page=-1' ) ) );
-		$this->cleanup->register_post_type();
 		$this->mock_admin_page_globals();
+		$this->cleanup->register_post_type();
 		$admin_page = $this->get_admin_page();
 		$posts_remaining = get_posts( 'post_type=cpt&posts_per_page=-1' );
 		$this->assertEquals( 5, count( $posts_remaining ) );
+	}
+
+	/**
+	 * Test deleting posts in batches without incorrectly deleting built in post types.
+	 *
+	 * @depends test_admin_page_deleting_posts_in_batches
+	 */
+	function test_admin_page_deleting_posts_in_batches_correct_post_type() {
+		$posts = $this->create_not_registered_post_type_posts( 'cpt', 5 );
+		$this->mock_admin_page_globals();
+		$this->cleanup->register_post_type();
+		$admin_page = $this->get_admin_page();
+		$this->assertEquals( 0, count( get_posts( 'post_type=cpt&posts_per_page=-1' ) ) );
+		$admin_page = $this->get_admin_page();
+		$this->assertEquals( 5, count( get_posts( 'posts_per_page=-1' ) ) );
 	}
 }
